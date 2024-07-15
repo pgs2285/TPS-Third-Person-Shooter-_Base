951b79f7e3d283272dc5463607957aeff42cbc0f
pgs2285 <wldnjs2284@naver.com>
2024-07-15 22:38:02 +0900

    aim 바라보기, 부메랑 fbx추가

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -5,13 +5,17 @@ public class playerManager : MonoBehaviour
 {
     private PlayerInputSystem input;
     
-
+    
     [Header("Aim")]
     [SerializeField]
     private CinemachineVirtualCamera aimCam;
     [SerializeField]
     private GameObject _crossHair;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private LayerMask targetLayer;
+    [SerializeField]
+    private GameObject aimObject;
+
     void Start()
     {
         input = GetComponent<PlayerInputSystem>();
@@ -26,6 +30,26 @@ public class playerManager : MonoBehaviour
 
     private void AimCheck(bool isAim)
     {
+        if (isAim)
+        {
+            Transform camTransform = Camera.main.transform;
+            Vector3 targetPosition = Vector3.zero;
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit hit, Mathf.Infinity))
+            {// 카메라에서 전진방향으로 무한
+                targetPosition = hit.point;
+                aimObject.transform.position = hit.point; // 디버깅용 구체 이.
+            }
+            else
+            {
+                targetPosition = camTransform.position + camTransform.forward;
+                aimObject.transform.position = camTransform.position + camTransform.forward;
+            }
+            Vector3 targetAim = targetPosition;
+            targetAim.y = transform.position.y;
+            Vector3 aimDir = (targetAim - transform.position).normalized; // 방향벡터 생성
+            //transform.LookAt(hitPoint);
+            transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * 30);
+        }
         aimCam.gameObject.SetActive(isAim);
         _crossHair.gameObject.SetActive(isAim);
         
